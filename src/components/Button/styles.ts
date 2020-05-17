ce3b6823987df88b5dba860b22182f7deb02d736
Leonardo S. Matos <matos_leo95@live.com>
2020-05-17 12:10:33 -0300

    Primeira versão. página de login

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+import {shade} from 'polished';
+
+export const Container = styled.button`
+
+background: #FF9000;
+height: 56px;
+width: 100%;
+border-radius: 10px;
+border: 0;
+padding: 0 16px;
+color: #312E38;
+font-weight: 500;
+margin-top: 16px;
+transition: background-color 0.2;
+
+&:hover {
+  background: ${shade(0.2, '#FF9000')};
+}
+
+`;
