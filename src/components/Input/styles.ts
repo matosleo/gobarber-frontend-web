f72ef3873165d286201f54b115a7d9120d5e41ba
Leonardo S. Matos <matos_leo95@live.com>
2020-05-18 23:46:59 -0300

    validação de campos de formulário de cadastro

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,19 +1,39 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
+import Tootip from '../Tooltip';
 
-export const Container = styled.div`
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+  isErrored: boolean;
+}
 
+export const Container = styled.div<ContainerProps>`
+
+display: flex;
 background: #232129;
 border-radius: 10px;
 border: 2px solid #232129;
 padding: 16px;
 width: 100%;
 color: #666360;
-text-align: left;
 
 & + div {
-    margin-top: 8px;
-  }
+  margin-top: 8px;
+}
+
+${props => props.isErrored && css`
+  border-color: #C53030;
+`}
+
+${props => props.isFocused && css`
+  color: #FF9000;
+  border-color: #FF9000;
+`}
+
+${props => props.isFilled && css`
+  color: #FF9000;
+`}
 
 input {
   background: transparent;
@@ -29,4 +49,22 @@ input {
 svg {
   margin-right: 16px;
 }
+`;
+
+export const Error = styled(Tootip)`
+  margin-left: 16px;
+  height: 20px;
+
+  svg {
+    margin: 0;
+  }
+
+  span {
+     background: #C53030;
+     color: #FFF;
+
+     &::before {
+       border-color: #C53030 transparent;
+     }
+  }
 `;
