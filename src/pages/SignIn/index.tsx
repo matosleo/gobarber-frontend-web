3068a088fa1ad82558ddf5364d4c35e8cf4eb97d
Leonardo S. Matos <matos_leo95@live.com>
2020-05-20 00:48:27 -0300

    Add toast messages

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -2,9 +2,10 @@ import React, {useRef, useCallback} from 'react';
 import {FiLogIn, FiMail, FiLock} from 'react-icons/fi';
 import {FormHandles} from "@unform/core";
 import {Form} from '@unform/web';
-
 import * as Yup from 'yup';
-import {useAuth} from '../../hooks/AuthContext';
+
+import {useAuth} from '../../hooks/auth';
+import {useToast} from '../../hooks/toast';
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import logoImg from '../../assets/logo.svg';
@@ -23,6 +24,7 @@ const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
   const {signIn} = useAuth();
+  const {addToast} = useToast();
 
   const handleSubmit = useCallback(async (data: SignInFormData) => {
     try {
@@ -35,15 +37,23 @@ const SignIn: React.FC = () => {
 
       await schema.validate(data, {abortEarly: false});
 
-      signIn({
+      await signIn({
         email: data.email,
         password: data.password
       });
     } catch(err) {
-      const errors = getValidationErrors(err);
-      formRef.current?.setErrors(errors);
+      if(err instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(err);
+        formRef.current?.setErrors(errors);
+      }
+
+      addToast({
+        type: 'error',
+        title: 'Erro na autenticação',
+        description: 'Ocorreu um erro ao fazer login, cheque as credenciais.',
+      });
     }
-  }, [signIn]);
+  }, [signIn, addToast]);
 
   return(
   <Container>
