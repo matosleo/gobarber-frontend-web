b223eda8f9afa73f78d7dad994524fa645aad236
Leonardo S. Matos <matos_leo95@live.com>
2020-05-22 00:50:37 -0300

    Add rotas para outras páginas e chamada de cadsdastro de usuário

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, {keyframes} from 'styled-components';
 import {shade} from 'polished';
 import signInBackgroundImg from '../../assets/sign-in-background.png';
 
@@ -8,7 +8,6 @@ display: flex;
 align-items: stretch;
 `;
 export const Content = styled.div`
-
 display: flex;
 flex-direction: column;
 align-items: center;
@@ -16,6 +15,27 @@ place-content: center;
 
 width: 100%;
 max-width: 700px;
+`;
+
+const appearFromLeft = keyframes`
+  from  {
+    opacity: 0;
+    transform: translateX(-50%);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`;
+
+export const AnimationContainer = styled.div`
+
+display: flex;
+flex-direction: column;
+align-items: center;
+place-content: center;
+
+animation: ${appearFromLeft} 1s;
 
 form {
   margin: 08px 0;
@@ -40,25 +60,29 @@ form {
 }
 
 > a {
-    color: #FF9000;
-    display: block;
-    margin-top: 24px;
-    text-decoration: none;
-    transition: color 0.2;
-    display: flex;
-    align-items: center;
+  color: #FF9000;
+  display: block;
+  margin-top: 24px;
+  text-decoration: none;
+  transition: color 0.2;
+  display: flex;
+  align-items: center;
 
-    svg {
-      margin-right: 16px;
-    }
-
-    &:hover {
-      color: ${shade(0.2, '#FF9000')};
-    }
+  svg {
+    margin-right: 16px;
   }
+
+  &:hover {
+    color: ${shade(0.2, '#FF9000')};
+  }
+}
 `;
+
+
 export const Background = styled.div`
 flex: 1;
 background: url(${signInBackgroundImg}) no-repeat center;
 background-size: cover;
 `;
+
+
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -3,6 +3,10 @@ import {FiArrowLeft, FiMail, FiLock, FiUser} from 'react-icons/fi';
 import {FormHandles} from '@unform/core';
 import {Form} from '@unform/web';
 import * as Yup from 'yup';
+import {Link, useHistory} from 'react-router-dom';
+
+import api from '../../services/api';
+import {useToast} from '../../hooks/toast';
 
 import getValidationErrors from '../../utils/getValidationErrors';
 
@@ -11,12 +15,20 @@ import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-import {Container, Content, Background} from './styles';
+import {Container, Content, Background, AnimationContainer} from './styles';
+
+interface SignUpFormData {
+  name: string;
+  email: string;
+  password: string;
+}
 
 const SignUp: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
+  const {addToast} = useToast();
+  const history = useHistory();
 
-  const handleSubmit = useCallback(async (data: object) => {
+  const handleSubmit = useCallback(async (data: SignUpFormData) => {
 
     try {
       formRef.current?.setErrors({});
@@ -28,37 +40,59 @@ const SignUp: React.FC = () => {
       });
 
       await schema.validate(data, {abortEarly: false});
+
+      await api.post('/users', data);
+
+      history.push('/');
+
+      addToast({
+        type: 'success',
+        title: 'Cadastro realizado!',
+        description: 'Você já pode fazer seu logon no GoBarber'
+      });
+
     } catch(err) {
-      const errors = getValidationErrors(err);
-      formRef.current?.setErrors(errors);
+      if(err instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(err);
+        formRef.current?.setErrors(errors);
+
+        return;
+      }
+
+      addToast({
+        type: 'error',
+        title: 'Erro no cadastro',
+        description: 'Ocorreu um erro ao fazer cadastro, tente novamente.',
+      });
     }
-  }, []);
+  }, [history, addToast]);
 
   return (
     <Container>
-    <Background />
+      <Background />
+      <Content>
+        <AnimationContainer>
+          <img src={logoImg} alt="GoBarber" />
 
-    <Content>
-    <img src={logoImg} alt="GoBarber" />
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1>Faça seu cadastro</h1>
 
-    <Form ref={formRef} onSubmit={handleSubmit}>
-      <h1>Faça seu cadastro</h1>
+            <Input name="name" icon={FiUser} placeholder="Nome" />
 
-      <Input name="name" icon={FiUser} placeholder="Nome" />
+            <Input name="email" icon={FiMail} placeholder="E-mail" />
 
-      <Input name="email" icon={FiMail} placeholder="E-mail" />
+            <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
 
-      <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
+            <Button type="submit">Cadastrar</Button>
 
-      <Button type="submit">Cadastrar</Button>
+          </Form>
 
-    </Form>
-
-    <a href="login">
-    <FiArrowLeft />
-    Voltar para logon
-    </a>
-    </Content>
+          <Link to="/">
+          <FiArrowLeft />
+          Voltar para logon
+          </Link>
+        </AnimationContainer>
+      </Content>
     </Container>
     )
   };
